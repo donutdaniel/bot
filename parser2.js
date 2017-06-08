21040a5ba74a08d254378a2cad507b3359468d57
Daniel Ho <danielho54@gmail.com>
2017-06-07 22:10:09 -0700

    testing

--- parser2.js
+++ parser2.js
@@ -1,13 +1,15 @@
 var story = require("./story");
 var fs = require("fs");
 var stream = require("stream");
+var transform  =  stream.Transform;
 // tag variables
 var tags = ["segment", "option", "jump"];
 
 /*Parses the file and returns a story object*/
 function parser(ifile){
-	var data = fs.createReadStream(ifile);
-	data.pipe(process.stdout);
+	var readable = fs.createReadStream(ifile);
+	var temp;
+	readable.pipe(process.stdout);
 }
 
 module.exports = parser;
