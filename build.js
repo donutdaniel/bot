3b7ebce5f4401f29f9069453aabd8428bc9c3df9
Daniel Ho <danielho54@gmail.com>
2017-06-13 20:48:48 -0700

    build fixes

--- build.js
+++ build.js
@@ -16,73 +16,88 @@ function build(structure){
         }
     };
     var appID, uPath, optionsPath;
-
     // add app
     options.path = '/luis/v1.0/prog/apps/';
     var reqAddApp = https.request(options, (res) => {
         res.on('data', (d) => {
-            appID = d.toString();
-console.log("Adding App: " + d.toString());
-        });
-        uPath = '/luis/v1.0/prog/apps/' + appID;
-        optionsPath = [uPath+'/intents', uPath+'/example', uPath+'/train', uPath+'/publish'];
+            var dJSON = JSON.parse(d);
+            if(dJSON.error != undefined){
+                throw Error(dJSON.error.message);
+            }else{
+                console.log("Adding App: " + d.toString());
+            }
+            appID = d.toString().replace(/"/g, "");
+            uPath = '/luis/v1.0/prog/apps/' + appID;
+            optionsPath = [uPath+'/intents', uPath+'/examples', uPath+'/train', uPath+'/publish'];
 
-        // create intents
-        structure.segments.forEach((value, key, map) => {
-            value.options.forEach((value_o, key_o, map_o) => {
-                var intentJSON = {
-                    "name": key_o
-                }
-                options.path = optionsPath[0];
-                var reqAddIntent = https.request(options, (res) => {
-res.on('data', (d) => {
-    console.log("Adding Intent ["+key_o+"]: " + d.toString());
-});
-                    // add utterances
-                    for(var i = 0; i < value_o.triggers.length; i++){
-                        var utteranceJSON = {
-                            "exampletext": value_o.triggers[i],
-                            "selectedintentname": key_o
-                        }
-                        options.path = optionsPath[1];
-                        var reqAddExample = https.request(options, (res) => {
-res.on('data', (d) => {
-    console.log("Adding Utterance ["+value_o.triggers[i]+"]: " + d.toString());
-});
+            // create intents
+            structure.segments.forEach((value, key, map) => {
+                value.options.forEach((value_o, key_o, map_o) => {
+                    options.path = optionsPath[0];
+                    var reqAddIntent = https.request(options, (res) => {
+                        res.on('data', (d) => {  
+                            var dJSON = JSON.parse(d);
+                            if(dJSON.error != undefined){
+                                console.log("Adding Intent ["+key_o+"]: ERROR: " + dJSON.error.message); 
+                            }else{
+                                console.log("Adding Intent ["+key_o+"]: " + d.toString());
+                            }
+
+                            // add utterances in batches
+                            var utteranceJSON = [{"selectedintentname": key_o}];
+                            for(var i = 0; i < value_o.triggers.length; i++){
+                                var exampletextObj = new Object();
+                                exampletextObj.exampletext = value_o.triggers[i];
+                                utteranceJSON.push(exampletextObj);
+                            }
+                            options.path = optionsPath[1];
+                            var reqAddExamples = https.request(options, (res) => {
+                                res.on('data', (d) => {
+                                    console.log("Adding Utterances ["+utteranceJSON[0].exampletext+"]: " + d.toString());
+                                });
+                            });
+                            reqAddExamples.write(JSON.stringify(utteranceJSON));
+                            reqAddExamples.end();
                         });
-                        reqAddExample.write(JSON.stringify(utteranceJSON));
-                        reqAddExample.end();
+                    });
+                    var intentJSON = {
+                        "name": key_o
                     }
+                    reqAddIntent.on('error', (e) => {
+                        console.error("problem with request:" + e.toString());
+                    });
+                    reqAddIntent.write(JSON.stringify(intentJSON));
+                    reqAddIntent.end();
                 });
-                reqAddIntent.write(JSON.stringify(intentJSON));
-                reqAddIntent.end();
             });
+
+            // train
+            // options.path = optionsPath[2];
+            // https.request(options, (res) => {
+            //     res.on('data', (d) => {
+            //         console.log("Training: " + d.toString());
+            //     });
+            //     // publish
+            //     options.path = optionsPath[3];
+            //     https.request(options, (res) =>{
+            //         res.on('data', (d) => {
+            //             var url = d[0].toString() + '?subscription-key=' + d[1].toString() + '&timezoneOffset=0&verbose=true&q=';
+            //             console.log("Publishing: " + d.toString());
+            //             console.log(url);
+            //             return url;
+            //         });
+            //     }).end();
+            // }).end();
         });
-
-        // train
-        options.path = optionsPath[2];
-        https.request(options, (res) => {
-res.on('data', (d) => {
-    console.log("Training: " + d.toString());
-});
-            options.path = optionsPath[3];
-            https.request(options, (res) =>{
-                res.on('data', (d) => {
-                    var url = d[0].toString() + '?subscription-key=' + d[1].toString() + '&timezoneOffset=0&verbose=true&q=';
-console.log("Publishing: " + d.toString());
-                    console.log(url);
-                    return url;
-                });
-            }).end();
-        }).end();
-
-    // publish
     });
     var appJSON = {
         "name": structure.name,
         "description": structure.description,
         "culture": "en-us"
     }
+    reqAddApp.on('error', (e) => {
+        console.error("problem with request:" + e.toString());
+    });
     reqAddApp.write(JSON.stringify(appJSON));
     reqAddApp.end();
 }
@@ -101,8 +116,28 @@ function deleteApp(appID){
         res.on('data', (d) => {
             console.log(d.toString());
         });
-    })
+    }).end();
 }
+
+// var options1 = {
+//     hostname: 'westus.api.cognitive.microsoft.com',
+//     port: 443,
+//     path: '/luis/v1.0/prog/apps/79a1aa4a-cea8-4626-b29b-77f69f3428bf/intents',
+//     method: 'POST',
+//     headers: {
+//         'Ocp-Apim-Subscription-Key': process.env.API_KEY,
+//     }
+// };
+// var testjson = {
+//     "name": "testintent"
+// }
+// var reqtest = https.request(options1, (res) => {
+//     res.on('data', (d) => {
+//         console.log(d.toString());
+//     });
+// });
+// reqtest.write(JSON.stringify(testjson));
+// reqtest.end();
 
 exports.build = build;
 exports.delete = deleteApp;
