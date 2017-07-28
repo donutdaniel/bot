e959c8069680d47d75d353d5c9f9c7bde04a3466
Daniel Ho <danielho54@gmail.com>
2017-07-27 17:57:49 -0700

    early testing stages of signup -- linked with sql

--- webpage/server.js
+++ webpage/server.js
@@ -2,11 +2,10 @@ require('dotenv-extended').load();
 var express = require('express');
 var app = express();
 var router = express.Router();
-
-// custom js
-var sqltools = require('../util/sqltools.js');
-
-// mysql setup
+// middleware
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+// mysql databasing setup
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: process.env.SQL_HOST,
@@ -14,7 +13,6 @@ var connection = mysql.createConnection({
 	password: process.env.SQL_PASS,
 	database: process.env.SQL_DB
 });
-
 connection.connect(function(err){
 	if(err){
 		console.log('mysql error');
@@ -22,20 +20,38 @@ connection.connect(function(err){
 		console.log('mysql connected');
 	}
 });
+// custom js
+var sqltools = require('../util/sqltools.js');
 
-// express setup
+
+// express setup and middleware
 var path = __dirname + '/';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
 
+// Landing Page
 app.get('/', function(req, res){
-	res.sendFile(path + 'index.html');
+	res.sendFile(path + 'views/index.html');
 });
 
-// middleware for extracting story
-// var getStory = function(req, res, next){
-// 	next();
-// }
-// app.use(getStory);
+// Create new user
+app.get('/signup', function(req, res){
+	res.sendFile(path + 'views/signup.html');
+});
+app.post('/signup', function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	connection.query('INSERT INTO users (username, password) VALUES (' + mysql.escape(username) + ', ' + mysql.escape(password) + ')', function(err, result){
+		if(err){
+			console.log('insertion error: ' + err.code);
+		}else{
+			console.log('successful user insert');
+		}
+	});
+});
 
+// User Account Page
 app.get('/:user/', function(req, res){
 	var user = req.params.user;
 	if(user === 'favicon.ico'){
@@ -43,7 +59,7 @@ app.get('/:user/', function(req, res){
 	}
 	connection.query('SELECT * FROM users WHERE username =' + mysql.escape(user), function(err, result, fields){
 		if(err){
-			console.log(err);
+			console.log('retrieve error: ' + err.code);
 			res.send('error retrieving data');
 		}else{
 			console.log('successful query');
@@ -56,6 +72,7 @@ app.get('/:user/', function(req, res){
 	});
 });
 
+// Specific Model Page
 app.get('/:user/:id', function(req, res){
 	var user = req.params.user;
 	var id = req.params.id;
@@ -65,7 +82,7 @@ app.get('/:user/:id', function(req, res){
 	}
 	connection.query("SELECT * FROM stories WHERE id =" + mysql.escape(id) + " AND fk_user =" + mysql.escape(user), function(err, result, fields){
 		if(err){
-			console.log(err);
+			console.log('retrieve error: ' + err.code);
 			res.send('error retrieving data');
 		}else{
 			console.log('successful query');
