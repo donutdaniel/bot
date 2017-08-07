2b79b5f4b0fd9ea198837cc35d3f55a8803b7e67
Daniel Ho <danielho54@gmail.com>
2017-08-07 16:03:13 -0700

    navbar

--- webpage/views/scripts/graph.js
+++ webpage/views/scripts/graph.js
@@ -39,7 +39,63 @@ var draw = function(){
 	    nodes: nodes,
 	    edges: edges
 	};
-	var options = {};
+	var locales = {
+	  en: {
+	    edit: 'Edit',
+	    del: 'Delete selected',
+	    back: 'Back',
+	    addNode: 'Add Node',
+	    addEdge: 'Add Edge',
+	    editNode: 'Edit Node',
+	    editEdge: 'Edit Edge',
+	    addDescription: 'Click in an empty space to place a new node.',
+	    edgeDescription: 'Click on a node and drag the edge to another node to connect them.',
+	    editEdgeDescription: 'Click on the control points and drag them to a node to connect to it.',
+	    createEdgeError: 'Cannot link edges to a cluster.',
+	    deleteClusterError: 'Clusters cannot be deleted.',
+	    editClusterError: 'Clusters cannot be edited.'
+	  }
+	}
+	var options = {
+		autoResize: true,
+		height: '100%',
+		width: '100%',
+		locale: 'en',
+		clickToUse: false,
+		interaction: {
+	    dragNodes:true,
+	    dragView: false,
+	    hideEdgesOnDrag: false,
+	    hideNodesOnDrag: false,
+	    hover: false,
+	    hoverConnectedEdges: true,
+	    keyboard: {
+	      enabled: true,
+	      speed: {x: 10, y: 10, zoom: 0.02},
+	      bindToWindow: true
+	    },
+	    multiselect: true,
+	    navigationButtons: true,
+	    selectable: true,
+	    selectConnectedEdges: true,
+	    tooltipDelay: 300,
+	    zoomView: true
+		},
+		manipuation: {
+	    enabled: true,
+	    initiallyActive: false,
+	    addNode: true,
+	    addEdge: true,
+	    editNode: undefined,
+	    editEdge: true,
+	    deleteNode: true,
+	    deleteEdge: true,
+	    controlNodeStyle:{
+	      // all node options are valid.
+	    }
+		},
+		physics: {}
+	};
 	// init network
 	var network = new vis.Network(container, data, options);
 }
